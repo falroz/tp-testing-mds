802071965dbd0cc3f6a7b7cc9e56597ebc6aab8c
Felipe Alarcon <felipe.alarcon.rozo@gmail.com>
2023-06-06 15:32:21 +0200

    feat : test api

---
+++ app/cypress/e2e/api/users.cy.js
@@ -0,0 +1,23 @@
+context('GET /users', () => {
+	it('gets a list of users', () => {
+		cy.request('GET', 'https://reqres.in/api/users').then((response) => {
+			expect(response.status).to.eq(200)
+		})
+	})
+})
+
+context('GET /users/id', () => {
+	it('gets a user', () => {
+		cy.request('GET', 'https://reqres.in/api/users/4').then((response) => {
+			expect(response.status).to.eq(200)
+		})
+	})
+})
+
+context('GET /users/id', () => {
+	it('gets a user error', () => {
+		cy.request('GET', 'https://reqres.in/api/users/hello').then((response) => {
+			expect(response.status).to.eq(400)
+		})
+	})
+})
