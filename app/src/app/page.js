218ee85099dec64713698c3149adc86463c001c7
Felipe Alarcon <felipe.alarcon.rozo@gmail.com>
2023-06-06 11:51:52 +0200

    feat : add login form

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -1,3 +1,9 @@
+import LoginForm from '@/components/login-form'
+
 export default function Home() {
-	return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+	return (
+		<main className="grid place-items-center">
+			<LoginForm />
+		</main>
+	)
 }
