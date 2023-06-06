deead2ec479619055e4dcdd0771d59ca6cfb65c3
Felipe Alarcon <felipe.alarcon.rozo@gmail.com>
2023-06-06 14:18:29 +0200

    feat : add form login

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -1,9 +1,26 @@
-import LoginForm from '@/components/login-form'
+'use client'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+	const [user, setUser] = useState(null)
+	useEffect(() => {
+		const token = localStorage.getItem('token')
+		if (token) setUser(token)
+		console.log('token', token)
+	}, [])
+
 	return (
-		<main className="grid place-items-center">
-			<LoginForm />
+		<main className="grid place-items-center h-screen">
+			{user ? (
+				<Link className="py-2 px-4 bg-blue-500" href={'/users'}>
+					Users
+				</Link>
+			) : (
+				<Link className="py-2 px-4 bg-blue-500" href={'/login'}>
+					Login / Sing up
+				</Link>
+			)}
 		</main>
 	)
 }
--- app/src/components/login-form.jsx
+++ app/src/components/login-form.jsx
@@ -1,49 +1,128 @@
 'use client'
-import Image from 'next/image'
+
 import { useState } from 'react'
 
 export default function LoginForm() {
 	const [showLogin, setShowLogin] = useState(true)
+	const [error, setError] = useState('')
+
+	const handleSubmit = async ({ type, e }) => {
+		e.preventDefault()
+		const formData = new FormData(e.target)
+		const data = Object.fromEntries(formData)
+		if (type === 'register') {
+			register(data)
+		} else {
+			login(data)
+		}
+	}
+
+	const register = async ({ password, email }) => {
+		try {
+			const res = await fetch('https://reqres.in/api/register', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					// 'Content-Type': 'application/x-www-form-urlencoded',
+				},
+				body: JSON.stringify({ password, email }),
+			})
+
+			const { id, token } = await res.json()
+			if (token) {
+				localStorage.setItem('token', token)
+				window.location.href = '/users/'
+			} else {
+				throw new Error('Invalid email or password')
+			}
+		} catch (err) {
+			setError(error.message)
+		}
+	}
+
+	const login = async ({ password, email }) => {
+		try {
+			const res = await fetch('https://reqres.in/api/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ password, email }),
+				cache: 'no-cache',
+			})
+
+			console.log(res)
+			const { token } = await res.json()
+			if (token) {
+				localStorage.setItem('token', token)
+				window.location.href = '/users/'
+			} else {
+				throw new Error('Invalid email or password')
+			}
+		} catch (error) {
+			setError(error.message)
+		}
+	}
+
 	return (
-		<section class="bg-gray-50 dark:bg-gray-900">
-			<div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-				<div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+		<section className="bg-gray-50 dark:bg-gray-900">
+			<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+				<div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
 					{showLogin ? (
-						<LoginFormComponent showRegister={() => setShowLogin(false)} />
+						<LoginForHtmlforHtmlmComponent
+							showRegister={() => setShowLogin(false)}
+							onSubmit={handleSubmit}
+						/>
 					) : (
-						<RegisterFormComponent showLogin={() => setShowLogin(true)} />
+						<RegisterForHtmlforHtmlmComponent
+							showLogin={() => setShowLogin(true)}
+							onSubmit={handleSubmit}
+						/>
 					)}
+					{error && <div className="text-red-500 p-6">{error}</div>}
+					<div className="p-6">
+						<p className="text-sm font-light text-gray-500 dark:text-gray-400">
+							{showLogin ? 'Dont have an account?' : 'Do you have an account?'}
+							<button
+								onClick={() => setShowLogin(!showLogin)}
+								className="font-medium text-primary-600 hover:underline dark:text-primary-500 ml-2"
+							>
+								{showLogin ? 'Sing up' : 'Sing in'}
+							</button>
+						</p>
+					</div>
 				</div>
 			</div>
 		</section>
 	)
 }
 
-function LoginFormComponent({ showRegister }) {
+function LoginForHtmlforHtmlmComponent({ onSubmit }) {
 	return (
-		<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-			<h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+		<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+			<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
 				Sign in to your account
 			</h1>
-			<form class="space-y-4 md:space-y-6" action="#">
+			<form
+				className="space-y-4 md:space-y-6"
+				action="#"
+				onSubmit={(e) => onSubmit({ type: 'login', e })}
+			>
 				<div>
-					<label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+					<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
 						Your email
 					</label>
 					<input
 						type="email"
 						name="email"
 						id="email"
-						class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						placeholder="name@company.com"
 						required=""
 					/>
 				</div>
 				<div>
-					<label
-						for="password"
-						class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-					>
+					<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
 						Password
 					</label>
 					<input
@@ -51,57 +130,48 @@ function LoginFormComponent({ showRegister }) {
 						name="password"
 						id="password"
 						placeholder="••••••••"
-						class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						required=""
 					/>
 				</div>
 
 				<button
 					type="submit"
-					class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+					className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
 				>
 					Sign in
 				</button>
-				<p class="text-sm font-light text-gray-500 dark:text-gray-400">
-					Don’t have an account yet?
-					<a
-						onClick={() => showRegister()}
-						href="#"
-						class="font-medium text-primary-600 hover:underline dark:text-primary-500"
-					>
-						Sign up
-					</a>
-				</p>
 			</form>
 		</div>
 	)
 }
 
-function RegisterFormComponent({ showLogin }) {
+function RegisterForHtmlforHtmlmComponent({ onSubmit }) {
 	return (
-		<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-			<h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+		<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+			<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
 				Sign up
 			</h1>
-			<form class="space-y-4 md:space-y-6" action="#">
+			<form
+				className="space-y-4 md:space-y-6"
+				action="#"
+				onSubmit={(e) => onSubmit({ type: 'register', e })}
+			>
 				<div>
-					<label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+					<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
 						Your email
 					</label>
 					<input
 						type="email"
 						name="email"
 						id="email"
-						class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						placeholder="name@company.com"
 						required=""
 					/>
 				</div>
 				<div>
-					<label
-						for="password"
-						class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-					>
+					<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
 						Password
 					</label>
 					<input
@@ -109,27 +179,17 @@ function RegisterFormComponent({ showLogin }) {
 						name="password"
 						id="password"
 						placeholder="••••••••"
-						class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						required=""
 					/>
 				</div>
 
 				<button
 					type="submit"
-					class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+					className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
 				>
 					Sign up
 				</button>
-				<p class="text-sm font-light text-gray-500 dark:text-gray-400">
-					Do you have an account?
-					<a
-						onClick={() => showLogin()}
-						href="#"
-						class="font-medium text-primary-600 hover:underline dark:text-primary-500"
-					>
-						Sign in
-					</a>
-				</p>
 			</form>
 		</div>
 	)
