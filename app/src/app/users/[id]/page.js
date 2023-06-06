f498a233bec555d2a8fee4e74565d0f65edee2fa
Julien Anquetil <anquetil.jul@gmail.com>
2023-06-06 14:18:04 +0200

    feat: :sparkles: check token

--- app/src/app/users/[id]/page.js
+++ app/src/app/users/[id]/page.js
@@ -1,18 +1,29 @@
+'use client';
+
+import { useEffect, useState } from "react";
 
 export default async function User({params}) {
+    const [user, setUser] = useState({})
 
-    const res = await fetch(`https://reqres.in/api/users/${params.id}`)
-    const user = await res.json();
+    useEffect(() => {
+        if (!localStorage.getItem('token')) {
+            window.location.href = '/'
+        }
+
+        fetch('https://reqres.in/api/users/${params.id}')
+        .then(res => res.json())
+        .then(({data}) => setUser(data))
+    }, [])
 
 	return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
             <div className="py-4 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
                 <a href="/users" className="text-left text-blue-500 hover:text-blue-700">⬅ Retour</a>
                 <div className="mt-4 mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
-                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{user.data.first_name} {user.data.last_name}</h2>
-                    <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">{user.data.email}</p>
+                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{user.first_name} {user.last_name}</h2>
+                    <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">{user.email}</p>
                 </div> 
-                <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src={user.data.avatar} alt="Bonnie Avatar" />
+                <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src={user.avatar} alt="Bonnie Avatar" />
             </div>
         </main>
     )
