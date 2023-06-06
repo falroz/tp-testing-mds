213a5e827861c1fec9fbfd08cabe2b277057839c
Julien Anquetil <anquetil.jul@gmail.com>
2023-06-06 13:13:54 +0200

    feat: :sparkles: button go back & logout

--- app/src/app/users/[id]/page.js
+++ app/src/app/users/[id]/page.js
@@ -4,12 +4,11 @@ export default async function User({params}) {
     const res = await fetch(`https://reqres.in/api/users/${params.id}`)
     const user = await res.json();
 
-    console.log(user)
-
 	return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
-                <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+            <div className="py-4 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
+                <a href="/users" className="text-left text-blue-500 hover:text-blue-700">⬅ Retour</a>
+                <div className="mt-4 mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{user.data.first_name} {user.data.last_name}</h2>
                     <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">{user.data.email}</p>
                 </div> 
